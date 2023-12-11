834761f5b074d07752ea5150509506b276b33cb0
kosyachniy <alexypoloz@gmail.com>
2023-12-11 13:28:03 +0300

    Fix

--- userhub/__init__.py
+++ userhub/__init__.py
@@ -2,13 +2,14 @@
 Initializing the Python package
 """
 
-from .main import detect_type, auth
+from .main import detect_type, auth, token
 
 
-__version__ = '0.6'
+__version__ = '0.7'
 
 __all__ = (
     '__version__',
     'detect_type',
     'auth',
+    'token',
 )
